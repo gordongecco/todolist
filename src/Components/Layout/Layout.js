6faa2aa88564258188e0e16e1c5cbea9c1b48efe
gordongecco <delldani001@gmail.com>
2019-05-22 09:09:58 +0200

    05.22

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,8 +1,15 @@
 import * as React from "react";
-
+import { Route } from "react-router-dom";
 const Layout = props => {
-  const { component: Component } = props;
-  return <Component />;
+  const { login: Login, list: List } = props;
+  return (
+    <div>
+      <Route exact path="/" component={() => <h1>Home</h1>} />
+      <Route path="/login" component={Login} />
+
+      <Route path="/list" component={List} />
+    </div>
+  );
 };
 
 export default Layout;
--- src/Scenes/List/ListScene.js
+++ src/Scenes/List/ListScene.js
@@ -1,10 +1,7 @@
-
-import React from 'react';
+import React from "react";
 
 function ListScene() {
-  return (
-   <h1>List</h1>
-  );
+  return <h1>List</h1>;
 }
 
 export default ListScene;
