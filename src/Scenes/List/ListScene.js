79a9b73118110f8024f12bff98ebe922cc4f3409
gordongecco <delldani001@gmail.com>
2019-05-23 10:03:16 +0200

    05.23

--- src/Scenes/List/ListScene.js
+++ src/Scenes/List/ListScene.js
@@ -2,9 +2,11 @@ import React from "react";
 import store from "../../store";
 
 function ListScene() {
-  console.log(store.getState().Validate);
-  if (store.getState().Validate) return <h1>List</h1>;
-  else return <h1>No Validation</h1>;
+  if (store.getState().Validate.validate) {
+    return <h1>List</h1>;
+  } else {
+    return <h1>No Validation</h1>;
+  }
 }
 
 export default ListScene;
