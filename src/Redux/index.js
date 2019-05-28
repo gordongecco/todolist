b997828e0685c53c3bc3c707bbf7076a459e15a0
gordongecco <delldani001@gmail.com>
2019-05-28 15:07:46 +0200

    05.28

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,11 +1,11 @@
 export {
   default as LoginDataReducer
 } from "../Components/LoginForm/LoginFormState";
-export { default as IsValidateReducer } from "./IsValidate";
-export { default as TodoListReducer } from "./TodoList";
 export {
   default as ModalInputReducer
 } from "../Components/ModalInput/ModalInputState";
 export {
   default as ModalLoginFormReducer
 } from "../Components/ModalLoginForm/ModalLoginFormState";
+export { default as IsValidateReducer } from "./IsValidate";
+export { default as TodoListReducer } from "./TodoList";
