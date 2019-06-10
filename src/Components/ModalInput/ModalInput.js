0b2b4b531c33cea688a2dfc013187514eeaa31f0
gordongecco <gordongecco77@gmail.com>
2019-06-10 17:07:25 +0200

    06.10

--- src/Components/ModalInput/ModalInput.js
+++ src/Components/ModalInput/ModalInput.js
@@ -2,81 +2,87 @@ import * as React from "react";
 import { Field, reduxForm } from "redux-form";
 import { Modal, Form } from "../componentsFromSemantic";
 
-const ModalInput = props => {
-  console.log(props.todos);
-  console.log(props.index);
-  console.log(props.openModal);
-  props.initialize({ modaliteminput: "ffff" });
-  // props.change("modaliteminput", "ddddd");
-  function onSubmit(e) {
-    const data = e.currentTarget.elements["modaliteminput"].value;
-    console.log(data);
-    props.changeItem(data, props.index);
-    props.setModal();
-  }
-  return (
-    <Modal basic size="mini" open={props.openModal}>
-      <Modal.Content>
-        <p>New todo :</p>
-        <Form onSubmit={onSubmit}>
-          <Field
-            name="modaliteminput"
-            component="input"
-            // value={props.todos[props.index]}
-            type="text"
-          />
-          <button type="submit">Save</button>
-        </Form>
-      </Modal.Content>
-    </Modal>
-  );
-};
-
-// class ModalInput extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.onSubmit = this.onSubmit.bind(this);
-//     console.log(props.todos);
-//     console.log(props.index);
-//     props.change("modaliteminput", "ddddd");
-//   }
-//   componentDidMount() {
-//    g(this.props.index);
-//     // this.props.initialize({
-//     //   modaliteminput: this.props.todos[this.props.index]
-//     // });
-//   }
-
-//   onSubmit(e) {
+// const ModalInput = props => {
+//   console.log(props.todos);
+//   console.log(props.index);
+//   console.log(props.openModal);
+//   // props.initialize({ modaliteminput: "ffff" });
+//   // props.change("modaliteminput", "ddddd");
+//   function onSubmit(e) {
 //     const data = e.currentTarget.elements["modaliteminput"].value;
-//     // console.log(data);
-//     this.props.changeItem(data, this.props.index);
-//     this.props.setModal();
+//     console.log(data);
+//     props.changeItem(data, props.index);
+//     props.setModal();
+//     // props.clear();
+//     props.reset();
 //   }
-//   render() {
-//     return (
-//       <Modal basic size="mini" open={this.props.openModal}>
-//         <Modal.Content>
-//           <p>New todo :</p>
-//           <Form onSubmit={this.onSubmit}>
-//             <Field
-//               name="modaliteminput"
-//               component="input"
-//               defaultlValues={this.props.todos[this.props.index]}
-//               type="text"
-//             />
-//             <button type="submit">Save</button>
-//           </Form>
-//         </Modal.Content>
-//       </Modal>
-//     );
-//   }
-// }
+//   return (
+//     <Modal basic size="mini" open={props.openModal}>
+//       <Modal.Content>
+//         <p>New todo :</p>
+//         <Form onSubmit={onSubmit}>
+//           <Field
+//             name="modaliteminput"
+//             component="input"
+//             // value={props.todos[props.index]}
+//             type="text"
+//           />
+//           <button type="submit">Save</button>
+//         </Form>
+//       </Modal.Content>
+//     </Modal>
+//   );
+// };
+
+class ModalInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentWillMount() {
+    // this.props.initialize({
+    //   modaliteminput: this.props.todos[this.props.index]
+    // });
+  }
+
+  onOpen() {
+    alert("SSS");
+    this.props.change("modaliteminput", this.props.todos[this.props.index]);
+  }
+
+  onSubmit(e) {
+    console.log(this.props.todos);
+    console.log(this.props.index);
+    const data = e.currentTarget.elements["modaliteminput"].value;
+    // console.log(data);
+    this.props.changeItem(data, this.props.index);
+    this.props.setModal();
+    this.props.reset();
+  }
+  render() {
+    return (
+      <Modal basic size="mini" open={this.props.openModal} onOpen={this.onOpen}>
+        <Modal.Content>
+          <p>New todo :</p>
+          <Form onSubmit={this.onSubmit}>
+            <Field
+              name="modaliteminput"
+              component="input"
+              defaultlValues={this.props.todos[this.props.index]}
+              type="text"
+            />
+            <button type="submit">Save</button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
 
 const ModalInputReduxForm = reduxForm(
   {
-    form: "modalInputForm",
-    enableReinitialize: true
+    form: "modalInputForm"
+    // enableReinitialize: true
   }
   // ,
   // {
--- src/Components/ModalInput/ModalInputR.js
+++ src/Components/ModalInput/ModalInputR.js
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import { clearFields, reset } from "redux-form";
 
 import ModalInput from "./ModalInput";
 import { setModal, changeItem } from "../../Redux/allActions";
@@ -7,11 +8,18 @@ const mapStateToProps = state => ({
   openModal: state.ModalInput.visible,
   index: state.ModalInput.index,
   todos: state.TodoList.todoList
+  // initialValues: { modaliteminput: "gggg" }
+  // initialValues: {
+  //   modaliteminput: state.TodoList.todoList[0]
+  // }
 });
 
 const mapDispatchToProps = dispatch => ({
   setModal: () => dispatch(setModal(null)),
-  changeItem: (todoItem, index) => dispatch(changeItem(todoItem, index))
+  changeItem: (todoItem, index) => dispatch(changeItem(todoItem, index)),
+  clear: () =>
+    dispatch(clearFields("modalInputForm", false, false, "modaliteminput")),
+  reset: () => dispatch(reset("modalInputForm"))
 });
 
 export default connect(
