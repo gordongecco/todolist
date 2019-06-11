7ca4fdfd099738f657ecd4fe1738ca1ebdba37e3
gordongecco <delldani001@gmail.com>
2019-06-11 11:36:15 +0200

    06.11

--- src/Components/ModalInput/ModalInputR.js
+++ src/Components/ModalInput/ModalInputR.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { clearFields, reset } from "redux-form";
+import { clearFields } from "redux-form";
 
 import ModalInput from "./ModalInput";
 import { setModal, changeItem } from "../../Redux/allActions";
@@ -8,18 +8,13 @@ const mapStateToProps = state => ({
   openModal: state.ModalInput.visible,
   index: state.ModalInput.index,
   todos: state.TodoList.todoList
-  // initialValues: { modaliteminput: "gggg" }
-  // initialValues: {
-  //   modaliteminput: state.TodoList.todoList[0]
-  // }
 });
 
 const mapDispatchToProps = dispatch => ({
   setModal: () => dispatch(setModal(null)),
   changeItem: (todoItem, index) => dispatch(changeItem(todoItem, index)),
   clear: () =>
-    dispatch(clearFields("modalInputForm", false, false, "modaliteminput")),
-  reset: () => dispatch(reset("modalInputForm"))
+    dispatch(clearFields("modalInputForm", false, false, "modaliteminput"))
 });
 
 export default connect(
