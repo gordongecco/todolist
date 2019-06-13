94dcb8fc36c21e32c0c35ec75d34a274833563f1
gordongecco <delldani001@gmail.com>
2019-06-13 16:27:33 +0200

    06.11

--- src/Db.js
+++ src/Db.js
@@ -1,53 +1,45 @@
-async function fetchData(token) {
-  let response = await fetch("http://localhost:8080/", {
-    headers: {
-      Authorization: token
-    }
-  });
+// async function fetchData(token) {
+//   let response = await fetch("http://localhost:8080/", {
+//     headers: {
+//       Authorization: token
+//     }
+//   });
 
-  if (response.status == 200) {
-    let data = response.json();
-    return data;
-  } else {
-    return null;
-  }
-}
+//   if (response.status == 200) {
+//     let data = response.json();
+//     return data;
+//   } else {
+//     return null;
+//   }
+// }
 
-async function sendData(arrayList, token) {
-  let data = { array: arrayList };
+// async function sendData(arrayList, token) {
+//   let data = { array: arrayList };
 
-  fetch("http://localhost:8080/", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token
-    }
-  });
-}
+//   fetch("http://localhost:8080/", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: token
+//     }
+//   });
+// }
 
 async function sendLoginData(data) {
   let response = await fetch("http://localhost:8080/login", {
     method: "POST",
-    body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json"
-    }
+    },
+    body: JSON.stringify(data)
   });
 
-  // if (response.status == 200) {
-  //   let t = await response.text();
-  //   let r = JSON.parse(t);
-  //   console.log(r.token);
-
-  //   return r.token;
-  // } else return null;
-
   if (response.status == 200) {
-    response.text().then(text => {
-      return JSON.parse(text).token;
-    });
+    let t = await response.json();
+
+    return t.token;
   } else return null;
 }
 
-export { sendData, sendLoginData, fetchData };
+export { sendLoginData };
